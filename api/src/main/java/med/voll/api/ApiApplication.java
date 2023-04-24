80b8ff43294987649a94150d9cc57b55e30eeb92
Wander <wanderson.souto@ccc.ufcg.edu.br>
2023-04-24 14:01:27 -0300

    subindo a solução do problema com o repositorio

--- api/src/main/java/med/voll/api/ApiApplication.java
+++ api/src/main/java/med/voll/api/ApiApplication.java
@@ -3,14 +3,12 @@ package med.voll.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 
 }
